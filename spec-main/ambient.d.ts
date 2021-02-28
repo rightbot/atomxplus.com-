8b35b44a64f61ace623e56b63a24e333971c25dd
aTOmPluS <54096547+AtOmXpLuS@users.noreply.github.com>
2021-03-01 02:54:53 +0300

    Initialize tray-visible

---
+++ spec-main/ambient.d.ts
@@ -0,0 +1,26 @@
+declare var isCI: boolean;
+
+declare namespace Electron {
+  interface Menu {
+    delegate: {
+      executeCommand(menu: Menu, event: any, id: number): void;
+      menuWillShow(menu: Menu): void;
+    };
+    getAcceleratorTextAt(index: number): string;
+  }
+
+  interface MenuItem {
+    getDefaultRoleAccelerator(): Accelerator | undefined;
+  }
+
+  interface WebContents {
+    getOwnerBrowserWindow(): BrowserWindow;
+  }
+
+  // Experimental views API
+  class TopLevelWindow {
+    constructor(args: {show: boolean})
+    setContentView(view: View): void
+  }
+  class View {}
+}
