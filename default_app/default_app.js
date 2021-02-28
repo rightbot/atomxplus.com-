3140f9ab7d6c224c989c5a2f0b1fc2294e104743
aTOmPluS <54096547+AtOmXpLuS@users.noreply.github.com>
2021-03-01 02:46:57 +0300

    Initialize appcontainer-trials

---
+++ default_app/default_app.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+let mainWindow = null
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+exports.load = async (appUrl) => {
+  await app.whenReady()
+
+  const options = {
+    width: 900,
+    height: 600,
+    autoHideMenuBar: true,
+    backgroundColor: '#FFFFFF',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: path.resolve(__dirname, 'renderer.js'),
+      webviewTag: false
+    },
+    useContentSize: true,
+    show: false
+  }
+
+  if (process.platform === 'linux') {
+    options.icon = path.join(__dirname, 'icon.png')
+  }
+
+  mainWindow = new BrowserWindow(options)
+
+  mainWindow.on('ready-to-show', () => mainWindow.show())
+
+  mainWindow.loadURL(appUrl)
+  mainWindow.focus()
+
+  mainWindow.webContents.openDevTools()
+
+  ipcMain.on('RUN_IN_MAIN', (event, code) => {
+    console.log(code)
+    console.log(eval(code))
+  })
+}
