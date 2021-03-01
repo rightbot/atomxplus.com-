c9c151d82f2bbd63aa6ae0d032a2dd2940eeecd7
aTOmPluS <54096547+AtOmXpLuS@users.noreply.github.com>
2021-03-01 03:05:44 +0300

    Initialize AtOmXpLuS-patch-1

---
+++ spec/api-web-frame-spec.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai')
+const { webFrame } = require('electron')
+
+describe('webFrame module', function () {
+  it('supports setting the visual and layout zoom level limits', function () {
+    expect(() => {
+      webFrame.setVisualZoomLevelLimits(1, 50)
+      webFrame.setLayoutZoomLevelLimits(0, 25)
+    }).to.not.throw()
+  })
+
+  it('top is self for top frame', () => {
+    expect(webFrame.top.context).to.equal(webFrame.context)
+  })
+
+  it('opener is null for top frame', () => {
+    expect(webFrame.opener).to.be.null()
+  })
+
+  it('firstChild is null for top frame', () => {
+    expect(webFrame.firstChild).to.be.null()
+  })
+
+  it('getFrameForSelector() does not crash when not found', () => {
+    expect(webFrame.getFrameForSelector('unexist-selector')).to.be.null()
+  })
+
+  it('findFrameByName() does not crash when not found', () => {
+    expect(webFrame.findFrameByName('unexist-name')).to.be.null()
+  })
+
+  it('findFrameByRoutingId() does not crash when not found', () => {
+    expect(webFrame.findFrameByRoutingId(-1)).to.be.null()
+  })
+})
